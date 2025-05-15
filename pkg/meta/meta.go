dfa3d4cf73936c6502073d9c2b292ac2743aa9ae
WindowsSov8forUs <qwertyuiop2333@hotmail.com>
2025-05-16 01:19:06 +0800

    fix: 使用指针

--- pkg/meta/meta.go
+++ pkg/meta/meta.go
@@ -4,6 +4,6 @@ import "github.com/satori-protocol-go/satori-model-go/pkg/login"
 
 // 元信息
 type Meta struct {
-	Logins    []login.Login `json:"logins"`     // 登录信息
-	ProxyUrls []string      `json:"proxy_urls"` // 代理路由 列表
+	Logins    []*login.Login `json:"logins"`     // 登录信息
+	ProxyUrls []string       `json:"proxy_urls"` // 代理路由 列表
 }
