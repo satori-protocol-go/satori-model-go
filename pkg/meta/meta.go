3a44ae8c98b8019e21b5cdb21ecf4035b16674bc
WindowsSov8forUs <qwertyuiop2333@hotmail.com>
2025-05-16 01:11:52 +0800

    update: 添加元信息类

---
+++ pkg/meta/meta.go
@@ -0,0 +1,9 @@
+package meta
+
+import "github.com/satori-protocol-go/satori-model-go/pkg/login"
+
+// 元信息
+type Meta struct {
+	Logins    []login.Login `json:"logins"`     // 登录信息
+	ProxyUrls []string      `json:"proxy_urls"` // 代理路由 列表
+}
